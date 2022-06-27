369b304a0976e22a2c5c49bd803f95c7697fda7d
Steve Tu <stevetu11@yahoo.com>
2022-06-26 21:46:16 -0700

    Initial work done for tests with 0-2

--- android/app/src/main/java/com/steve1316/gfl_android_auto/bot/Game.kt
+++ android/app/src/main/java/com/steve1316/gfl_android_auto/bot/Game.kt
@@ -7,6 +7,8 @@ import androidx.preference.PreferenceManager
 import com.steve1316.gfl_android_auto.MainActivity.loggerTag
 import com.steve1316.gfl_android_auto.StartModule
 import com.steve1316.gfl_android_auto.data.ConfigData
+import com.steve1316.gfl_android_auto.data.PlanningModeData
+import com.steve1316.gfl_android_auto.data.SetupData
 import com.steve1316.gfl_android_auto.utils.*
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
@@ -24,6 +26,9 @@ class Game(private val myContext: Context) {
 	val configData: ConfigData = ConfigData(myContext)
 	val imageUtils: ImageUtils = ImageUtils(myContext, this)
 	val gestureUtils: MyAccessibilityService = MyAccessibilityService.getInstance()
+
+	val maxChapterNumber: Int = 11
+	var echelonDeploymentNumber: Int = 1
 
 	/**
 	 * Returns a formatted string of the elapsed time since the bot started as HH:MM:SS format.
@@ -94,10 +99,15 @@ class Game(private val myContext: Context) {
 	 */
 	fun findAndPress(imageName: String, tries: Int = 0, suppressError: Boolean = false): Boolean {
 		if (configData.debugMode) {
-			printToLog("[DEBUG] Now attempting to find and click the \"$imageName\" button.")
+			printToLog("[DEBUG] Now attempting to find and press the \"$imageName\" button.")
 		}
 
-		val tempLocation: Point? = imageUtils.findImage(imageName, tries = tries, suppressError = suppressError)
+		val tempLocation: Point? = if (tries > 0) {
+			imageUtils.findImage(imageName, tries = tries, suppressError = suppressError)
+		} else {
+			imageUtils.findImage(imageName, suppressError = suppressError)
+		}
+
 		return if (tempLocation != null) {
 			if (configData.enableDelayTap) {
 				val newDelay: Double = ((configData.delayTapMilliseconds - 100)..(configData.delayTapMilliseconds + 100)).random().toDouble() / 1000
@@ -119,7 +129,7 @@ class Game(private val myContext: Context) {
 	 */
 	private fun landscapeCheck() {
 		if (MediaProjectionService.displayHeight > MediaProjectionService.displayWidth) {
-			Log.d(tag, "Virtual Display is not correct. Recreating it now...")
+			Log.d(tag, "Virtual display is not correct. Recreating it now...")
 			MediaProjectionService.forceGenerateVirtualDisplay(myContext)
 		} else {
 			Log.d(tag, "Skipping recreation of Virtual Display as it is correct.")
@@ -140,6 +150,291 @@ class Game(private val myContext: Context) {
 			}
 		}
 	}
+
+	/**
+	 * Deploys a echelon on the already selected node.
+	 *
+	 * @param echelonNumber The required echelon to deploy.
+	 * @return True if the echelon was deployed.
+	 */
+	private fun deployEchelon(echelonNumber: Int): Boolean {
+		// If the echelon cannot be found, then find the nearest one and scroll up or down the list.
+		var echelonLocation = imageUtils.findImage("echelon$echelonDeploymentNumber")
+		if (echelonLocation != null) {
+			var tempEchelonNumber = 1
+			while (tempEchelonNumber <= 10) {
+				echelonLocation = imageUtils.findImage(
+					"echelon$tempEchelonNumber", region = intArrayOf(
+						0, 0, MediaProjectionService.displayWidth / 2,
+						MediaProjectionService.displayHeight
+					)
+				)
+
+				// Scroll the echelon list up or down.
+				if (echelonLocation != null) {
+					if (tempEchelonNumber < echelonNumber) {
+						printToLog("[DEPLOY_ECHELON] Nearest echelon of $tempEchelonNumber is less than the required echelon number so scrolling the list down.")
+						gestureUtils.swipe(
+							echelonLocation.x.toFloat(), MediaProjectionService.displayHeight.toFloat() / 2, echelonLocation.x.toFloat(),
+							(MediaProjectionService.displayHeight.toFloat() / 2) - 400
+						)
+					} else {
+						printToLog("[DEPLOY_ECHELON] Nearest echelon of $tempEchelonNumber is more than the required echelon number so scrolling the list up.")
+						gestureUtils.swipe(
+							echelonLocation.x.toFloat(), MediaProjectionService.displayHeight.toFloat() / 2, echelonLocation.x.toFloat(),
+							(MediaProjectionService.displayHeight.toFloat() / 2) + 400
+						)
+					}
+
+					// Wait for the scrolling animation to settle.
+					wait(2.0)
+
+					// Now check if the bot has the correct echelon in view.
+					echelonLocation = imageUtils.findImage(
+						"echelon$echelonNumber", confidence = 0.95,
+						region = intArrayOf(0, 0, MediaProjectionService.displayWidth / 2, MediaProjectionService.displayHeight)
+					)
+					if (echelonLocation != null) {
+						break
+					}
+				}
+
+				tempEchelonNumber++
+			}
+		}
+
+		if (echelonLocation != null && gestureUtils.tap(echelonLocation.x, echelonLocation.y, "node")) {
+			echelonDeploymentNumber++
+			return findAndPress("choose_echelon_ok")
+		} else throw Exception("Failed to deploy echelon $echelonDeploymentNumber in preparation phase.")
+	}
+
+	/**
+	 * Starts the process to verify and/or enter the correct map.
+	 *
+	 * @param mapName Name of the map to run.
+	 * @return True if the bot was able to enter the specified map.
+	 */
+	private fun enterMap(mapName: String): Boolean {
+		// Determine the correct chapter.
+		val episodeString = when (mapName) {
+			"0-2" -> {
+				"00"
+			}
+			////////////////// TODO: Implement the rest of the maps.
+			else -> {
+				""
+			}
+		}
+
+		if (episodeString == "") throw Exception("Invalid map name.")
+
+		// First check if the bot is at the correct episode. If not, navigate to it.
+		val epLocation = imageUtils.findImage("ep$episodeString", region = intArrayOf(0, 0, MediaProjectionService.displayWidth, MediaProjectionService.displayHeight / 2))
+		if (epLocation != null) {
+			printToLog("\n[INFO] We are at the correct episode.")
+		} else {
+			printToLog("\n[INFO] Bot is not at the correct episode. Navigating to Episode $episodeString...")
+			val chapterNumber: Int = mapName.split("-")[0].toInt()
+			navigateToCorrectEpisode(episodeString, chapterNumber) ?: throw Exception("Bot could not find the correct episode at the end.")
+		}
+
+		////////////////// TODO: Implement scrolling the map list if needed.
+
+		// Now that the correct episode is now active, select the map.
+		return findAndPress("map$mapName") && findAndPress("normal_battle")
+	}
+
+	/**
+	 * Navigate to the correct Episode.
+	 *
+	 * @param chapter String version of the Chapter that the Episode resides in.
+	 * @param chapterNumber Integer version of the Chapter.
+	 * @return Point object of the chapter button's location or null.
+	 */
+	private fun navigateToCorrectEpisode(chapter: String, chapterNumber: Int): Point? {
+		// Determine the location of the first found location of a episode by searching for its corresponding chapter button.
+		var tries = 10
+		while (tries > 0) {
+			val chapterButtonPairLocation = findNearestChapterButton()
+
+			if (chapterButtonPairLocation.first == -1 || chapterButtonPairLocation.second == null) throw Exception("Bot could not seem to find any episode locations.")
+			else if (chapterButtonPairLocation.first < chapterNumber) {
+				printToLog("[INFO] Nearest chapter button of ${chapterButtonPairLocation.second} is less than the required chapter number so scrolling the list down.")
+				gestureUtils.swipe(
+					chapterButtonPairLocation.second!!.x.toFloat(), MediaProjectionService.displayHeight.toFloat() / 2, chapterButtonPairLocation.second!!.x.toFloat(),
+					(MediaProjectionService.displayHeight.toFloat() / 2) - 400
+				)
+			} else {
+				printToLog("[INFO] Nearest chapter button of ${chapterButtonPairLocation.second} is more than the required chapter number so scrolling the list up.")
+				gestureUtils.swipe(
+					chapterButtonPairLocation.second!!.x.toFloat(), MediaProjectionService.displayHeight.toFloat() / 2, chapterButtonPairLocation.second!!.x.toFloat(),
+					(MediaProjectionService.displayHeight.toFloat() / 2) + 400
+				)
+			}
+
+			// Wait for the scrolling animation to settle.
+			wait(2.0)
+
+			// Now check if the bot has the correct chapter button in view.
+			val chapterButtonLocation = imageUtils.findImage("ch$chapter", confidence = 0.95, region = intArrayOf(0, 0, MediaProjectionService.displayWidth / 2, MediaProjectionService.displayHeight))
+			if (chapterButtonLocation != null) {
+				// If so, then press it.
+				gestureUtils.tap(chapterButtonLocation.x, chapterButtonLocation.y, "ch$chapter")
+				return chapterButtonLocation
+			}
+
+			tries -= 1
+		}
+
+		return null
+	}
+
+	/**
+	 * Finds the nearest chapter button.
+	 *
+	 * @return Pair object containing the number of the nearest chapter button and its Point object.
+	 */
+	private fun findNearestChapterButton(): Pair<Int, Point?> {
+		var chapterNumber = 0
+		var tries = 5
+		while (tries > 0) {
+			// Find the nearest unselected chapter button.
+			while (chapterNumber < maxChapterNumber) {
+				val chapterString: String = if (chapterNumber < 10) "0$chapterNumber" else "$chapterNumber"
+				val tempLocation = imageUtils.findImage(
+					"ch$chapterString", tries = 1, confidence = 0.95,
+					region = intArrayOf(0, 0, MediaProjectionService.displayWidth / 2, MediaProjectionService.displayHeight)
+				)
+				if (tempLocation != null) return Pair(chapterNumber, tempLocation) else chapterNumber++
+			}
+
+			// Reset the chapter number.
+			chapterNumber = 0
+			tries -= 1
+		}
+
+		return Pair(-1, null)
+	}
+
+	/**
+	 * Prepare for the combat operation by zooming in/out the map as needed and deploying echelons.
+	 *
+	 */
+	private fun prepareOperation() {
+		if (imageUtils.findImage(
+				"start_operation",
+				tries = 30,
+				region = intArrayOf(0, MediaProjectionService.displayHeight / 2, MediaProjectionService.displayWidth, MediaProjectionService.displayHeight / 2)
+			) == null
+		) {
+			throw Exception("Game failed to load into the map.")
+		}
+
+		printToLog("\n* * * * * * * * * * * * * * * * *")
+		printToLog("[PREPARATION] Starting preparation for operation.")
+
+		SetupData.setupSteps.forEach { init ->
+			when (init.action) {
+				"pinch_in" -> {
+					printToLog("[PREPARATION] Zooming in the map now...")
+					gestureUtils.zoom(MediaProjectionService.displayWidth / 2f, MediaProjectionService.displayHeight / 2f, init.spacing[0].toFloat(), init.spacing[1].toFloat())
+					wait(2.0)
+				}
+				"pinch_out" -> {
+					printToLog("[PREPARATION] Zooming out the map now...")
+					gestureUtils.zoom(MediaProjectionService.displayWidth / 2f, MediaProjectionService.displayHeight / 2f, init.spacing[0].toFloat(), init.spacing[1].toFloat())
+					wait(2.0)
+				}
+				"deploy_dummy" -> {
+					printToLog("[PREPARATION] Deploying dummy at (${init.coordinates[0]}, ${init.coordinates[1]})")
+					gestureUtils.tap(init.coordinates[0].toDouble(), init.coordinates[1].toDouble(), "node")
+					wait(1.0)
+					deployEchelon(echelonDeploymentNumber)
+				}
+				"deploy_echelon" -> {
+					printToLog("[PREPARATION] Deploying echelon at (${init.coordinates[0]}, ${init.coordinates[1]})")
+					gestureUtils.tap(init.coordinates[0].toDouble(), init.coordinates[1].toDouble(), "node")
+					wait(1.0)
+					deployEchelon(echelonDeploymentNumber)
+				}
+				else -> {
+					throw Exception("Invalid action")
+				}
+			}
+		}
+
+		printToLog("\n[PREPARATION] Finished preparation for operation.")
+		printToLog("* * * * * * * * * * * * * * * * *")
+	}
+
+	/**
+	 * Sets up the Planning Mode moves.
+	 *
+	 */
+	private fun setupPlanningMode() {
+		wait(2.0)
+
+		printToLog("\n= = = = = = = = = = = = = = = =")
+		printToLog("[SETUP_PLANNING_MODE] Laying out the moves for Planning Mode now...")
+
+		PlanningModeData.moves.forEach { move ->
+			when (move.action) {
+				"start" -> {
+					// Resupply DPS echelon.
+					gestureUtils.tap(move.coordinates[0].toDouble(), move.coordinates[1].toDouble(), "node")
+					wait(0.5)
+					gestureUtils.tap(move.coordinates[0].toDouble(), move.coordinates[1].toDouble(), "node")
+					findAndPress("resupply")
+
+					// Activate Planning Mode.
+					if (!findAndPress("planning_mode")) throw Exception("Unable to proceed with Planning Mode as the button was not found or was obscured by something else.")
+					gestureUtils.tap(move.coordinates[0].toDouble(), move.coordinates[1].toDouble(), "node")
+				}
+				"start_no_resupply" -> {
+					// Activate Planning Mode.
+					if (!findAndPress("planning_mode")) throw Exception("Unable to proceed with Planning Mode as the button was not found or was obscured by something else.")
+					gestureUtils.tap(move.coordinates[0].toDouble(), move.coordinates[1].toDouble(), "node")
+				}
+				"move" -> {
+					gestureUtils.tap(move.coordinates[0].toDouble(), move.coordinates[1].toDouble(), "node")
+				}
+			}
+		}
+
+		printToLog("\n[SETUP_PLANNING_MODE] Finished preparation for operation.")
+		printToLog("= = = = = = = = = = = = = = = =")
+	}
+
+	/**
+	 * Executes the Planning Mode and wait for the operation to end after some time of inactivity.
+	 *
+	 */
+	private fun executeOperation() {
+		// Press the button to execute the plan.
+		findAndPress("planning_mode_execute_plan")
+		printToLog("\n[EXECUTE_PLAN] Planning Mode is now being executed. Waiting for operation to end...")
+
+		var tries = 10
+		while (tries > 0) {
+			// If the End Round button vanished, then the bot might be in combat so wait for it to end before retrying checks.
+			if (imageUtils.waitVanish(
+					"end_round", region =
+					intArrayOf(0, MediaProjectionService.displayHeight / 2, MediaProjectionService.displayWidth, MediaProjectionService.displayHeight / 2), timeout = 3, suppressError = true
+				)
+			) {
+				printToLog("[EXECUTE_PLAN] The End Round button has vanished. Bot must be in combat so waiting 30 seconds for it to end before retrying checks...")
+				tries = 10
+				wait(30.0)
+			} else {
+				printToLog("[EXECUTE_PLAN] The End Round button is still here. Operation will be considered ended in $tries tries.")
+				tries -= 1
+			}
+		}
+
+		printToLog("\n[EXECUTE_PLAN] Stopping checks for operation end.")
+	}
+
 	/**
 	 * Bot will begin automation here.
 	 *
@@ -148,15 +443,27 @@ class Game(private val myContext: Context) {
 	fun start(): Boolean {
 		val startTime: Long = System.currentTimeMillis()
 
+		landscapeCheck()
+
+		wait(1.0)
+
 		if (configData.debugMode) {
 			printToLog("\n[DEBUG] I am starting here but as a debugging message!")
 		} else {
-			printToLog("\n[INFO] I am starting here!")
+			printToLog("\n[INFO] I am starting here!!!")
 		}
 
-		landscapeCheck()
+		enterMap(configData.mapName)
 
-		wait(0.5)
+		wait(3.0)
+
+		prepareOperation()
+
+		findAndPress("start_operation")
+
+		setupPlanningMode()
+
+		executeOperation()
 
 		printToLog("\n[INFO] I am ending here!")
 
