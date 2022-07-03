691cd0bfa7a34b1d7fb807b6b45a60f6ec99733d
Steve Tu <stevetu11@yahoo.com>
2022-07-02 19:24:08 -0700

    Add scrolling of available maps and add option to skip initial check

--- android/app/src/main/java/com/steve1316/gfl_android_auto/bot/Navigation.kt
+++ android/app/src/main/java/com/steve1316/gfl_android_auto/bot/Navigation.kt
@@ -4,7 +4,7 @@ import com.steve1316.gfl_android_auto.utils.MediaProjectionService
 import org.opencv.core.Point
 
 /**
- *
+ * This class handles navigation to and from the Combat screen.
  */
 class Navigation(val game: Game) {
 	private val tag = "[Navigation]"
@@ -13,9 +13,15 @@ class Navigation(val game: Game) {
 	 * Starts the process to verify and/or enter the correct map.
 	 *
 	 * @param mapName Name of the map to run.
+	 * @param skipInitialLocationCheck Skip the initial check for the Combat button on the Home screen. Defaults to false.
 	 * @return True if the bot was able to enter the specified map.
 	 */
-	fun enterMap(mapName: String): Boolean {
+	fun enterMap(mapName: String, skipInitialLocationCheck: Boolean = false): Boolean {
+		// Enter the Combat screen.
+		game.printToLog("\n[Navigation] Entering the Combat screen now...", tag = tag)
+		if (!skipInitialLocationCheck && !game.findAndPress("home_combat", tries = 30)) throw Exception("Failed to enter Combat screen from the Home screen.")
+		game.waitScreenTransition()
+
 		// Determine the correct chapter.
 		val episodeString = when (mapName) {
 			"0-2" -> {
@@ -32,9 +38,9 @@ class Navigation(val game: Game) {
 		// First check if the bot is at the correct episode. If not, navigate to it.
 		val epLocation = game.imageUtils.findImage("ep$episodeString", region = intArrayOf(0, 0, MediaProjectionService.displayWidth, MediaProjectionService.displayHeight / 2))
 		if (epLocation != null) {
-			game.printToLog("\n[INFO] We are at the correct episode.", tag = tag)
+			game.printToLog("\n[Navigation] We are at the correct episode.", tag = tag)
 		} else {
-			game.printToLog("\n[INFO] Bot is not at the correct episode. Navigating to Episode $episodeString...", tag = tag)
+			game.printToLog("\n[Navigation] Bot is not at the correct episode. Navigating to Episode $episodeString...", tag = tag)
 			val chapterNumber: Int = mapName.split("-")[0].toInt()
 			navigateToCorrectEpisode(episodeString, chapterNumber) ?: throw Exception("Bot could not find the correct episode at the end.")
 		}
@@ -42,7 +48,27 @@ class Navigation(val game: Game) {
 		////////////////// TODO: Implement scrolling the map list if needed.
 
 		// Now that the correct episode is now active, select the map.
-		return game.findAndPress("map$mapName") && game.findAndPress("normal_battle")
+		if (!game.findAndPress("map$mapName")) {
+			game.printToLog("\n[Navigation] Map $mapName was not found. Scrolling list of available maps...", tag = tag)
+			var tries = 3
+			while (tries > 0) {
+				game.gestureUtils.swipe(
+					MediaProjectionService.displayWidth.toFloat() / 2, MediaProjectionService.displayHeight.toFloat() / 2, MediaProjectionService.displayWidth.toFloat() / 2,
+					(MediaProjectionService.displayHeight.toFloat() / 2) - 400
+				)
+
+				// Wait for the scrolling animation to settle.
+				game.wait(2.0)
+
+				if (game.findAndPress("map$mapName")) {
+					break
+				}
+
+				tries -= 1
+			}
+		}
+
+		return game.findAndPress("normal_battle")
 	}
 
 	/**
@@ -60,13 +86,13 @@ class Navigation(val game: Game) {
 
 			if (chapterButtonPairLocation.first == -1 || chapterButtonPairLocation.second == null) throw Exception("Bot could not seem to find any episode locations.")
 			else if (chapterButtonPairLocation.first < chapterNumber) {
-				game.printToLog("[INFO] Nearest chapter button of ${chapterButtonPairLocation.second} is less than the required chapter number so scrolling the list down.", tag = tag)
+				game.printToLog("[Navigation] Nearest chapter button of ${chapterButtonPairLocation.second} is less than the required chapter number so scrolling the list down.", tag = tag)
 				game.gestureUtils.swipe(
 					chapterButtonPairLocation.second!!.x.toFloat(), MediaProjectionService.displayHeight.toFloat() / 2, chapterButtonPairLocation.second!!.x.toFloat(),
 					(MediaProjectionService.displayHeight.toFloat() / 2) - 400
 				)
 			} else {
-				game.printToLog("[INFO] Nearest chapter button of ${chapterButtonPairLocation.second} is more than the required chapter number so scrolling the list up.", tag = tag)
+				game.printToLog("[Navigation] Nearest chapter button of ${chapterButtonPairLocation.second} is more than the required chapter number so scrolling the list up.", tag = tag)
 				game.gestureUtils.swipe(
 					chapterButtonPairLocation.second!!.x.toFloat(), MediaProjectionService.displayHeight.toFloat() / 2, chapterButtonPairLocation.second!!.x.toFloat(),
 					(MediaProjectionService.displayHeight.toFloat() / 2) + 400
